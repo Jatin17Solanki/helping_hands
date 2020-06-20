91c6d20434d97443c55dde4b67fb0d29ea7d56bc
Jatin17Solanki <jatin.solanki1@learner.manipal.edu>
2020-06-20 20:59:23 +0530

    linked the backend

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,23 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Welcome"),
+    );
+  }
+}
